f102e28764b95d481f3bff227dfc239c0d55a7a4
Gustav <114870315+PlutoGamerpro@users.noreply.github.com>
2024-08-09 07:57:56 +0200

    Add files via upload

---
+++ QuizQuestion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace Quit_med_JSON_serialisering
+{
+
+ 
+        public class QuizQuestion
+        {
+            public string Question { get; set; }
+            public List<string> AnswerOptions { get; set; }
+            public int CorrectAnswer { get; set; }
+
+        public List<string> CorrectAnswerMessage { get; set; }   // Ny egenskab til den ekstra besked
+
+    }
+
+    public class Quiz
+    {
+        public List<QuizQuestion> Questions { get; set; }
+    }
+    
+        
+}
+
